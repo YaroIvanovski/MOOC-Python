5710accf887f6a9f75f2c716dd692dedfa5872a8
Yaroslav <yaroslav@outlook.sg>
2022-06-14 22:03:45 +0300

    MOOC

--- part07-04_lottery_numbers/src/lottery_numbers.py
+++ part07-04_lottery_numbers/src/lottery_numbers.py
@@ -1,14 +1,15 @@
-# Write your solution here
 from random import randint
+ 
 def lottery_numbers(amount: int, lower: int, upper: int):
-    list = []
-    i = 1
-    while (i <= amount):
+    numbers = []
+
+    while len(numbers) < amount:
         num = randint(lower, upper)
-        if num not in list:
-            list.append(num)
-        i += 1   
-    return sorted(list)
+        if num not in numbers:
+            numbers.append(num)
+          
+    return sorted(numbers)
+
 
 if __name__ == "__main__":
     for num in lottery_numbers(7, 1, 40):
